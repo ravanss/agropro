0e9bacfffd40960600b8046e0c52dbd61f50001b
ravanss <psaulo7@gmail.com>
2022-05-10 23:27:37 -0300

    Criando acfs da paginas de services e subindo o conteúdo

--- wp-content/themes/agropro/inc/services.php
+++ wp-content/themes/agropro/inc/services.php
@@ -1,12 +1,12 @@
-<!-- services -->
 <?php $services = get_page_by_title('Services'); ?>
+<!-- services -->
 <div class="services">
    <div class="container">
       <div class="row">
          <div class="col-md-12">
             <div class="titlepage text_align_left">
-               <span><?php the_field('services-title')?></span>
-               <h2><?php the_field('services-subtite')?></h2>
+               <span><?php the_field('services-title', $services)?></span>
+               <h2><?php the_field('services-subtite', $services)?></h2>
             </div>
          </div>
       </div>
@@ -16,13 +16,15 @@
          <div class="col-md-4">
             <div class="services_box_main">
                <div  class="services_box text_align_left">
-                  <figure><img src="images/service1.jpg" alt="#"/></figure>
+                  <figure><img src="<?php the_sub_field('services-img-fir')?>" alt="primeira imagem do servicos"/></figure>
                   <div class="veget">
-                     <h3>FRESH<br>VEGETABLES</h3>
-                     <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                     <h3><?php the_sub_field('services-card-title')?></h3>
+                     <p><?php the_sub_field('services-card-text')?></p>
                   </div>
                </div>
-               <a class="read_more" href="services.html">Read More</a>
+               <?php if(!is_page('Services')) {?>
+                  <a class="read_more" href="services.html">Leia mais</a>
+               <?php }?>
             </div>
          </div>
 
