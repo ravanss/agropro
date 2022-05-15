24ac6e1715618f015e31dcd9061cf3b34b3ac2c5
ravanss <psaulo7@gmail.com>
2022-05-15 20:37:11 -0300

    Criando campos dentro do wordpress do contact form, e alterando o style para se adequar ao plugin

--- wp-content/themes/agropro/inc/contact.php
+++ wp-content/themes/agropro/inc/contact.php
@@ -5,38 +5,17 @@
             <div class="row">
                <div class="col-md-12 ">
                   <div class="titlepage text_align_center">
-                     <span>Our Contact</span>
-                     <h2>Requste A Call Back</h2>
+                     <span>Nosso contato</span>
+                     <h2>Entre em contato</h2>
                   </div>
                </div>
                <div class="col-md-8 offset-md-2">
-                  <form id="request" class="main_form">
-                     <div class="row">
-                        <div class="col-md-12 ">
-                           <input class="form_control" placeholder="Your Name" type="type" name=" Name"> 
-                        </div>
-                        <div class="col-md-12">
-                           <input class="form_control" placeholder="Phone Number" type="type" name="Phone Number">                          
-                        </div>
-                          <div class="col-md-12">
-                           <input class="form_control" placeholder="Your Email" type="type" name="Email">                          
-                        </div>
-                        <div class="col-md-12">
-                           <input class="textarea" placeholder="Message" type="type" name="message"> 
-                        </div>
-                        <div class="col-md-12">
-                           <div class="group_btn">
-                           <button class="send_btn">Send</button>
-                            <button class="send_btn">location</button>
-                         </div>
-                        </div>
-                     </div>
-                  </form>
+                  <?php echo apply_shortcodes( '[contact-form-7 id="109" title="contato"]' ); ?>
                </div>
             </div>
          </div>
-          <div class="map-responsive">
+         <div class="map-responsive">
             <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France" width="600" height="430" frameborder="0" style="border:0; width: 100%;" allowfullscreen=""></iframe>
-         </div>
+         </div>	
       </div>
             <!-- end contact -->
